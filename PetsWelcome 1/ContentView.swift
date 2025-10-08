a94a98084eb2ac41d616205df358851f86565090
John Symmonds <symmondsj@gmail.com>
2025-10-08 12:46:26 +0200

    Initial Commit

---
+++ PetsWelcome 1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PetsWelcome 1
+//
+//  Created by John Marc Symmonds on 2025/10/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PetsWelcome 1/PetsWelcome_1App.swift
@@ -0,0 +1,17 @@
+//
+//  PetsWelcome_1App.swift
+//  PetsWelcome 1
+//
+//  Created by John Marc Symmonds on 2025/10/08.
+//
+
+import SwiftUI
+
+@main
+struct PetsWelcome_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
